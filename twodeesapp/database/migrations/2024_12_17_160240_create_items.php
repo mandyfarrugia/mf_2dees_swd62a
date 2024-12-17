dad8648eede7ff03bc91e4cae7bf0ebaf252c75c
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2024-12-17 16:06:41 +0000

    Demonstrated example of defining a nullable attribute.

--- twodeesapp/database/migrations/2024_12_17_160240_create_items.php
+++ twodeesapp/database/migrations/2024_12_17_160240_create_items.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('release_date')->nullable(); //It might not always be known the date in which the item was released.
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('items');
     }
 };

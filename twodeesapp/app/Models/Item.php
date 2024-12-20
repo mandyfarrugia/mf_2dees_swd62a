2e2bb63767543e85b3b0c0389d6973ced3a21e3e
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2024-12-20 13:05:01 +0000

    Category in singular instead of plural when linking Item to Category.

--- twodeesapp/app/Models/Item.php
+++ twodeesapp/app/Models/Item.php
@@ -12,7 +12,7 @@ class Item extends Model
     protected $fillable = ["name", "release_date", "category_id"]; //Technique to enable mass assignment.
 
     //Establishing a relationship between Item and Category model (one-to-many relationship whereby one category can be assigned to many items).
-    public function categories() {
+    public function category() {
         /* In this case, belongsTo assumes a one-to-one/many relationship between source and target table,
            the foreign key constraint should already exist in the source table (in this case, the Item table) to link to the common attribute within the target table 
            (in our case, the attribute within the Category model). */

7239217bf90854e19b1cf993e219f7e5db769c61
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2024-12-20 14:26:06 +0000

    Explored basic routing by redirecting to a simple route with a heading and information about an item record in JSON format.

--- twodeesapp/routes/web.php
+++ twodeesapp/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/items', function() {
+    return "<h1>All items</h1>";
+});
+
+Route::get('/items/{id}', function($id) {
+    return App\Models\Item::find($id);
+}); 

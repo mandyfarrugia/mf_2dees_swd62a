0828fd42b0f32e1efa0999c81add5907fcbbe933
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2024-12-20 06:14:27 +0000

    First approach towards relating Item and Category models.

--- twodeesapp/app/Models/Category.php
+++ twodeesapp/app/Models/Category.php
@@ -10,4 +10,9 @@ class Category extends Model
     use HasFactory;
 
     protected $fillable = ["name"]; //Technique to enable mass assignment yet again, although in the case overkill since model is only made up of one attribute apart from the id and timestamps.
+
+    //Establishing a relationship between Item and Category model (one-to-many relationship whereby one category can be assigned to many items).
+    public function items() {
+        return $this->hasMany(Item::class);
+    }
 }
--- twodeesapp/app/Models/Item.php
+++ twodeesapp/app/Models/Item.php
@@ -10,4 +10,9 @@ class Item extends Model
     use HasFactory;
 
     protected $fillable = ["name", "release_date", "category_id"]; //Technique to enable mass assignment.
+
+    //Establishing a relationship between Item and Category model (one-to-many relationship whereby one category can be assigned to many items).
+    public function categories() {
+        return $this->hasMany(Category::class);
+    }
 }

6e30cb39af37d1e75a2598143f00b440167ce057
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2024-12-23 16:08:19 +0000

    Massive mistake in master template layout, fixed

--- twodeesapp/resources/views/items/index.blade.php
+++ twodeesapp/resources/views/items/index.blade.php
@@ -1,3 +1,7 @@
-<h1>All items</h1>
-<a href="{{ route('items.create') }}">Add item</a> |
-<a href="{{ route('items.show', 1) }}">Show item</a>
+@extends('layouts.main');
+
+@section('content')
+    <h1>All items</h1>
+    <a href="{{ route('items.create') }}">Add item</a> |
+    <a href="{{ route('items.show', 1) }}">Show item</a>
+@endsection

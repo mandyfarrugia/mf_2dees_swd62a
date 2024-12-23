1cf021f46345cc83a914f78aaf2e03be632f610f
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2024-12-23 14:35:38 +0000

    Routing behaviour handled in controller now.

--- twodeesapp/app/Http/Controllers/ItemController.php
+++ twodeesapp/app/Http/Controllers/ItemController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
-    //
+    public function index() {
+        return view('items.index');
+    }
+
+    public function create() {
+        return view('items.create');
+    }
+
+    public function show($id) {
+        $item = Item::find($id);
+        return view('items.show', compact('item'));
+    }
 }
--- twodeesapp/routes/web.php
+++ twodeesapp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/items', function() {
-    return view('items.index');
-})->name('items.index');
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
 
-Route::get('/items/create', function() {
-    return view('items.create');
-})->name('items.create');
+Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
 
-Route::get('/items/{id}', function($id) {
-    $item = App\Models\Item::find($id);
-    return view('items.show', compact('item'));
-})->name('items.show');
+Route::get('/items/{id}', [ItemController::class, 'show'])->name('items.show');

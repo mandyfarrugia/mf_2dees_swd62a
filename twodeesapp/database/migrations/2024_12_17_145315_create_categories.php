b827994b0e7e07003d4012be2a362f5caae7c6c2
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-07 13:47:09 +0000

    Added image path attribute for both category and item respectively as it would be nice to accompany item and category details with an image.

--- twodeesapp/database/migrations/2024_12_17_145315_create_categories.php
+++ twodeesapp/database/migrations/2024_12_17_145315_create_categories.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('imagePath')->nullable(); //It might not always be the case that a category is supported by an image, hence the nullable column definition.
             $table->timestamps();
         });
     }
--- twodeesapp/database/migrations/2024_12_17_160240_create_items.php
+++ twodeesapp/database/migrations/2024_12_17_160240_create_items.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->date('release_date')->nullable(); //It might not always be known the date in which the item was released.
+            $table->string('imagePath')->nullable(); //It might not always be the case that an item comes with an image, hence the nullable column definition.
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });

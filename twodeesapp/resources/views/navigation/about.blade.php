4bd6811e03586e76dcebad720abe1b68b4732601
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-09 18:56:14 +0000

    Used PHP to traverse a list of favourite artist, display a cross sign next to anyone marked as deceased.

--- twodeesapp/resources/views/navigation/about.blade.php
+++ twodeesapp/resources/views/navigation/about.blade.php
@@ -12,11 +12,39 @@
                             <span class="section-heading-upper">Hello from Malta! 🇲🇹</span>
                             <span class="section-heading-lower">About TwoDees</span>
                         </h2>
-                        <p>I am Mandy Farrugia, aged <?= intval(date('Y', time() - strtotime('2001-12-05'))) - 1970 ?> (born 5 December 2001), massive foodie and music-head (Alan Parsons Project, Amy Winehouse <i class="fa-solid fa-cross"></i>, Ofra Haza <i class="fa-solid fa-cross"></i>, Two Steps From Hell, Nightwish, Hozier, U2, David Byrne, Talking Heads, Phil Collins, Genesis, Sabaton, Gloryhammer, and Colin Vearncombe/Black <i class="fa-solid fa-cross"></i>), and hailing from an island in the Mediterranean sea where the four seasons and privacy are a distant dream, and if you have not gotten the hint already, I have a penchant for Warhammer and gaming. When I am not reimaging assets and providing end-user support for a living or tinkering (pun-intended) with software development technologies, my notion of a personal heaven would be a comfortable gaming chair while enjoying Gears of War, Warhammer 40,000 Space Marine, Halo (Bungie Inc. > 343 Industries), Visage, Outlast, Red Dead Redemeption, and Dead Space... and last but not least, Turkish Döner kebab complemented by garlic &amp; yoghurt sauce and pita bread.</p>
+                        <p id="about_paragraph">I am Mandy Farrugia, aged <?= intval(date('Y', time() - strtotime('2001-12-05'))) - 1970 ?> (born 5 December 2001), massive foodie and music-head, and hailing from an island in the Mediterranean sea where the four seasons and privacy are a distant dream, and if you have not gotten the hint already, I have a penchant for Warhammer and gaming. When I am not reimaging assets and providing end-user support for a living or tinkering (pun-intended) with software development technologies, my notion of a personal heaven would be a comfortable gaming chair while enjoying Gears of War, Warhammer 40,000 Space Marine, Halo (Bungie Inc. > 343 Industries), Visage, Outlast, Red Dead Redemeption, and Dead Space... and last but not least, Turkish Döner kebab complemented by garlic &amp; yoghurt sauce and pita bread.</p>
                         <p class="mb-0"></p>
-                        <p>My programming journey commenced in November 2015 where I familiarised myself with programming lexicon using Java as part of the MATSEC O'Level programming coursework for the Computing unit, contributing to a Grade 1. This led me to further my studies at MCAST to pursue a career in software development, acquiring a Diploma in ICT <small>(MQF Level 3)</small> in 2019 and a Advanced Diploma in IT (Software Development) <small>(MQF Level 4)</small> in 2022. At the time of the development of this web application, I am following my second year of the Bachelor of Science (Honours) in Software Development.</p>
-                        <p class="mb-0">Since November 2022, I have been employed at the Ministry for the Economy, Enterprise and Strategic Projects (formerly Ministry for the Economy, European Funds and Lands) within the Information Management Unit. Duties include asset management (laptop and multifunction printers), end-user support, and website maintenance (namely Commerce Department, Malta Freeport, State Aid, NAB, RPC, and economy/ekonomija). Throughout the duration of the Stabbiltà scheme in 2024, I was responsible for the maintenance of its respective website.</p>
-                        <p class="mt-3">Since October 2021, I have provided free coding sessions related to object-oriented programming, C#, ASP.NET, web scraping using the Selenium library, PHP, Transact-SQL, and C as part of the annual European Code Week initiative.</p>
+                        <p id="about_paragraph">My programming journey commenced in November 2015 where I familiarised myself with programming lexicon using Java as part of the MATSEC O'Level programming coursework for the Computing unit, contributing to a Grade 1. This led me to further my studies at MCAST to pursue a career in software development, acquiring a Diploma in ICT <small>(MQF Level 3)</small> in 2019 and a Advanced Diploma in IT (Software Development) <small>(MQF Level 4)</small> in 2022. At the time of the development of this web application, I am following my second year of the Bachelor of Science (Honours) in Software Development.</p>
+                        <p class="mb-0" id="about_paragraph">Since November 2022, I have been employed at the Ministry for the Economy, Enterprise and Strategic Projects (formerly Ministry for the Economy, European Funds and Lands) within the Information Management Unit. Duties include asset management (laptop and multifunction printers), end-user support, and website maintenance (namely Commerce Department, Malta Freeport, State Aid, NAB, RPC, and economy/ekonomija). Throughout the duration of the Stabbiltà scheme in 2024, I was responsible for the maintenance of its respective website.</p>
+                        <p class="mt-3" id="about_paragraph">Since October 2021, I have provided free coding sessions related to object-oriented programming, C#, ASP.NET, web scraping using the Selenium library, PHP, Transact-SQL, and C as part of the annual European Code Week initiative.</p>
+                        <h4>Testimony to music-head statement</h4>
+                        <ul>
+                            @php
+                                $artists = array('Alan Parsons Project' => 'alive',
+                                    'Amy Winehouse' => 'deceased',
+                                    'Ofra Haza' => 'deceased',
+                                    'Two Steps From Hell' => 'alive',
+                                    'Nightwish' => 'alive',
+                                    'Hozier' => 'alive',
+                                    'U2' => 'alive',
+                                    'David Byrne' => 'alive',
+                                    'Talking Heads' => 'alive',
+                                    'Phil Collins' => 'alive',
+                                    'Genesis' => 'alive',
+                                    'Sabaton' => 'alive',
+                                    'Gloryhammer' => 'alive',
+                                    'Colin Vearncombe/Black' => 'deceased');
+                            @endphp
+
+                            @foreach($artists as $artist => $status)
+                                <li>
+                                    {{ $artist }}
+                                    @if($status === 'deceased')
+                                        <i class="fa-solid fa-cross"></i>
+                                    @endif
+                                </li>
+                            @endforeach
+                        </ul>
                     </div>
                 </div>
             </div>

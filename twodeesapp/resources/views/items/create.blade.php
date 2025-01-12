bd19e883c425acd0275faa940dd16861b1bbe358
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-12 19:58:06 +0000

    Added CSRF token.

--- twodeesapp/resources/views/items/create.blade.php
+++ twodeesapp/resources/views/items/create.blade.php
@@ -11,6 +11,7 @@
               </div>           
               <div class="card-body">
                 <form action="{{ route('items.store') }}" method="POST">
+                  @csrf
                   @include('items._form')
                 </form>
               </div>
--- twodeesapp/resources/views/layouts/main.blade.php
+++ twodeesapp/resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>TwoDees</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"

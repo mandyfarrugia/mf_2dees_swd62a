8f60766a9d680293079f0936865eec1de676faba
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-16 09:33:15 +0000

    Changed image for about

--- twodeesapp/resources/views/navigation/about.blade.php
+++ twodeesapp/resources/views/navigation/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <section class="page-section about-heading">
     <div class="container">
-        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="{{ asset('assets/img/thousand_sons.jpg') }}" alt="..." />
+        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="{{ asset('assets/img/lieutenant_titus.jpg') }}" alt="..." />
         <div class="about-heading-content">
             <div class="row">
                 <div class="col-xl-9 col-lg-10 mx-auto">

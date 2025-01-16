d81d2d930900911ff0ae72a58a1b15d76fd42bf9
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-16 14:49:50 +0000

    Working on OG Xbox section.

--- twodeesapp/resources/views/welcome.blade.php
+++ twodeesapp/resources/views/welcome.blade.php
@@ -31,4 +31,22 @@
             </div>
         </div>
     </section>
+    <section class="page-section">
+        <div class="container">
+            <div class="product-item">
+                <div class="product-item-title d-flex">
+                    <div class="bg-faded p-5 d-flex ms-auto rounded">
+                        <h2 class="section-heading mb-0">
+                            <span class="section-heading-upper"></span>
+                            <span class="section-heading-lower">Bulk Speciality Blends</span>
+                        </h2>
+                    </div>
+                </div>
+                <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="{{ asset('home/xbox_og.jpg') }}" alt="..." />
+                <div class="product-item-description d-flex me-auto">
+                    <div class="bg-faded p-5 rounded"><p class="mb-0">Real-time procedurally generated startup</p></div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection

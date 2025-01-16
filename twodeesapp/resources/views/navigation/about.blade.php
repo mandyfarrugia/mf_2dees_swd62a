147bb497bfdf649ab0b38669de3980c295621703
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-16 10:05:06 +0000

    Replacing images for about page

--- twodeesapp/resources/views/navigation/about.blade.php
+++ twodeesapp/resources/views/navigation/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <section class="page-section about-heading">
     <div class="container">
-        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="{{ asset('assets/img/lieutenant_titus.jpg') }}" alt="..." />
+        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="{{ asset('assets/img/halo_ce_ann.jpg') }}" alt="..." />
         <div class="about-heading-content">
             <div class="row">
                 <div class="col-xl-9 col-lg-10 mx-auto">

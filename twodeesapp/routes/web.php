2e30c6202194d6d5d95358d0e258543f4f3074af
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-17 11:49:50 +0000

    Defined route for updating data upon submission of form.

--- twodeesapp/routes/web.php
+++ twodeesapp/routes/web.php
@@ -12,6 +12,7 @@ Route::get('/items', [ItemController::class, 'index'])->name('items.index');
 Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
 Route::post('items', [ItemController::class, 'store'])->name('items.store');
 Route::get('/items/{id}', [ItemController::class, 'show'])->name('items.show');
-Route::get('items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
+Route::get('/items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
+Route::put('items/{id}', [ItemController::class, 'update'])->name('items.update');
 
 Route::get('/about', [NavigationController::class, 'about'])->name('navigation.about');

522a0552c00192c9fb23d56bbbba467f98fd46ca
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-22 15:50:39 +0000

    Added extra fields catering for new attributes.

--- twodeesapp/resources/views/authentication/_register_form.blade.php
+++ twodeesapp/resources/views/authentication/_register_form.blade.php
@@ -67,6 +67,32 @@
             </div>
         </div>
         <div class="form-group row">
+            <label for="birth_date" class="col-md-3 col-form-label">Date of birth</label>
+            <div class="col-md-9">
+                <input type="date" name="birth_date" id="birth_date" value="{{ old('birth_date') }}" class="form-control @error('birth_date') is-invalid @enderror">
+                @error('birth_date')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="location_id" class="col-md-3 col-form-label">Category</label>
+            <div class="col-md-9">
+                <select name="location_id" id="location_id" class="form-control @error('location_id') is-invalid @enderror">
+                    @foreach ($categories as $id => $name)
+                        <option {{ $id == old('location_id', $item->category_id) ? 'selected' : '' }} value="{{ $id }}">{{ $name }}</option>
+                    @endforeach
+                </select>
+                @error('location_id')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+        </div>
+        <div class="form-group row">
             <label for="profile_picture" class="col-md-3 col-form-label">Profile Picture</label>
             <div class="col-md-9">
                 <input type="file" name="profile_picture" id="profile_picture" class="form-control @error('profile_picture') is-invalid @enderror"/>

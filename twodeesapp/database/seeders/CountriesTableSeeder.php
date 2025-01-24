f768d93a6531de5ed17ce24684e9ec3c3568e40c
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-24 09:44:36 +0000

    Seeded country into database.

--- twodeesapp/database/seeders/CountriesTableSeeder.php
+++ twodeesapp/database/seeders/CountriesTableSeeder.php
@@ -2,16 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Country;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CountriesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Country::create(['name' => 'Malta']);
     }
 }

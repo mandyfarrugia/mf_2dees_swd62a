293433b418bb17289ecc2554ee730fb0d13ed43d
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-24 13:10:31 +0000

    Created seeder class dedicated to locations.

--- twodeesapp/database/seeders/RegionsTableSeeder.php
+++ twodeesapp/database/seeders/RegionsTableSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Country;
+use App\Models\Region;
 use Illuminate\Database\Seeder;
 
 class RegionsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $malta = Country::where('name', 'Malta')->first()->id;
+
+        $region_objects = [
+            ['name' => 'Eastern Region', 'country_id' => $malta],
+            ['name' => 'Gozo Region', 'country_id' => $malta],
+            ['name' => 'Northern Region', 'country_id' => $malta],
+            ['name' => 'Port Region', 'country_id' => $malta],
+            ['name' => 'Southern Region', 'country_id' => $malta],
+            ['name' => 'Western Region', 'country_id' => $malta]
+        ];
+
+        foreach($region_objects as $region_object) {
+            Region::create($region_object);
+        }
     }
 }

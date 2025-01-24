57d7d3454e7c21c725ee4bcc30f37b017dd9100f
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-24 15:10:05 +0000

    Included recent seeder in ->call.

--- twodeesapp/database/seeders/DatabaseSeeder.php
+++ twodeesapp/database/seeders/DatabaseSeeder.php
@@ -10,7 +10,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CountriesTableSeeder::class,
-            RegionsTableSeeder::class
+            RegionsTableSeeder::class,
+            LocationsTableSeeder::class
         ]);
     }
 }

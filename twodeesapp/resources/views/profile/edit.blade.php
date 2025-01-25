e72ef85674a095dfb837fa8c89f514f5fbd64500
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-25 14:10:33 +0000

    Created a partial view representing a form to edit a profile.

--- twodeesapp/resources/views/profile/edit.blade.php
+++ twodeesapp/resources/views/profile/edit.blade.php
@@ -6,13 +6,13 @@
                 <div class="col-md-8">
                     <div class="card">
                         <div class="card-header card-title">
-                            <strong>Edit Item</strong>
+                            <strong>Join us today!</strong>
                         </div>
                         <div class="card-body">
-                            <form action="{{ route('items.update', $item->id) }}" method="POST" enctype="multipart/form-data">
-                                @method('PUT')
+                            <form action="{{ route('authentication.register_post') }}" method="POST"
+                                enctype="multipart/form-data">
                                 @csrf
-                                @include('items._form')
+                                @include('profile._form')
                             </form>
                         </div>
                     </div>

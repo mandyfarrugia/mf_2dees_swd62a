5c3004fea733adaaa7d0dc11018fecdc9252fc1b
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-01-25 10:02:59 +0000

    Created form with a PUT request.

---
+++ twodeesapp/resources/views/profile/change_profile_picture.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+@section('content')
+    <main class="py-5">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header card-title">
+                            <strong>Update profile picture</strong>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('profile.process_profile_picture_update', $user->id) }}" method="POST" enctype="multipart/form-data">
+                                @method('PUT')
+                                @csrf
+                                @include('profile._upload_form')
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection

bcf02acd6e0f38a8fea5382bad1f99d44a6a2912
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-04-29 06:20:49 +0000

    Cards are of the same height.

--- twodeesapp/resources/views/navigation/users.blade.php
+++ twodeesapp/resources/views/navigation/users.blade.php
@@ -4,14 +4,14 @@
     <div class="container mt-5">
         <div class="row g-4">
             @foreach ($users as $user)
-                <div class="col-md-4">
-                    <div class="card border-0 shadow overflow-hidden rounded-3">
+                <div class="col-md-4 d-flex">
+                    <div class="card border-0 shadow overflow-hidden rounded-3 w-100 d-flex flex-column">
                         <div class="position-relative">
                             @if(file_exists($user->profile_picture))
-                                <img src="{{ asset($user->profile_picture) }}" class="card-img-top" alt="Image 1">
+                                <img src="{{ asset($user->profile_picture) }}" class="card-img-top w-100" alt="Image 1">
                             @endif
                         </div>
-                        <div class="card-body">
+                        <div class="card-body mt-auto">
                             <h5 class="card-title mb-1">{{ $user->username }}</h5>
                             <p class="card-text">Some quick example text to build on the card title.</p>
                         </div>

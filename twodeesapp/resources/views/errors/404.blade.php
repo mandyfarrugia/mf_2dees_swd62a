cc47b8768f5e1d8d557d0a800946e7351b61d57e
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-05-02 10:35:51 +0000

    Checkpoint

--- twodeesapp/resources/views/errors/404.blade.php
+++ twodeesapp/resources/views/errors/404.blade.php
@@ -10,8 +10,7 @@
                     <h1 id="error-404">404 - Page Not Found</h1>
                     <h2 id="error-404">You’ve wandered off the map... and there’s no fast travel available.</h2>
                     <p id="error-404">Looks like this URL is more hidden than an easter egg in <strong>Dark Souls</strong>.
-                        Try heading back to
-                        safer territory — or press <strong>F</strong> to pay respects.</p>
+                        Try heading back to safer territory — or press <strong>F</strong> to pay respects.</p>
                     <a class="btn btn-primary" href="{{ url('/') }}">
                         <i class="fa-solid fa-chevron-left"></i>
                         <span>Return to base</span>

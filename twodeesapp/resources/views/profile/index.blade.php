235ac128543370f5384d2eb38fb8d1c02fc6fc51
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-09-09 18:05:56 +0000

    Display happy birthday message.

--- twodeesapp/resources/views/profile/index.blade.php
+++ twodeesapp/resources/views/profile/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
-@section('title', Auth::check() ? ($user->id == auth()->user()->id ? 'Your profile' : $user->username) : $user->username)
+@section('title', Auth::check() ? ($user->id == auth()->user()->id ? 'Your profile' : $user->username) :
+    $user->username)
 @section('content')
     <section>
         <div class="container py-5">
@@ -8,40 +9,54 @@
                 <div class="col-lg-4">
                     <div class="card mb-4">
                         <div class="card-body text-center">
-                            @if(file_exists($user->profile_picture))
-                                <a href="{{ asset($user->profile_picture) }}" data-fancybox data-caption="{{ $user->name }} {{ $user->surname }}">
-                                    <img src="{{ asset($user->profile_picture) }}" class="rounded-circle shadow-4 img-fluid" style="width: 150px;" alt="{{ $user->username }}"/>
+                            @if (file_exists($user->profile_picture))
+                                <a href="{{ asset($user->profile_picture) }}" data-fancybox
+                                    data-caption="{{ $user->name }} {{ $user->surname }}">
+                                    <img src="{{ asset($user->profile_picture) }}" class="rounded-circle shadow-4 img-fluid"
+                                        style="width: 150px;" alt="{{ $user->username }}" />
                                 </a>
                             @else
-                                <img src="https://images.nightcafe.studio//assets/profile.png?tr=w-1600,c-at_max" class="rounded-circle shadow-4 img-fluid" style="width: 150px;" alt="No profile picture available"/>
+                                <img src="https://images.nightcafe.studio//assets/profile.png?tr=w-1600,c-at_max"
+                                    class="rounded-circle shadow-4 img-fluid" style="width: 150px;"
+                                    alt="No profile picture available" />
                             @endif
                             <h5 class="my-3">{{ $user->name }} {{ $user->surname }}</h5>
-                            @if(Auth::check())
-                                @if($user->id == auth()->user()->id)
-                                    @if(file_exists($user->profile_picture))
+                            @if (Auth::check())
+                                @if ($user->id == auth()->user()->id)
+                                    @if (file_exists($user->profile_picture))
                                         <div class="d-flex justify-content-center mb-2">
-                                            <a href="{{ route('profile.change_profile_picture', $user->id) }}" class="btn btn-primary btn-sm"><i class="fa-solid fa-user-large"></i> Update profile picture</a>
+                                            <a href="{{ route('profile.change_profile_picture', $user->id) }}"
+                                                class="btn btn-primary btn-sm"><i class="fa-solid fa-user-large"></i> Update
+                                                profile picture</a>
                                         </div>
                                         <div class="d-flex justify-content-center mb-2">
-                                            <form action="{{ route('profile.remove_profile_picture', $user->id) }}" method="POST" enctype="multipart/form-data">
+                                            <form action="{{ route('profile.remove_profile_picture', $user->id) }}"
+                                                method="POST" enctype="multipart/form-data">
                                                 @method('PUT')
                                                 @csrf
-                                                <a id="btn_remove_profile_picture" class="btn btn-outline-secondary btn-sm ms-1"><i class="fa-solid fa-xmark"></i> Remove profile picture</a>
+                                                <a id="btn_remove_profile_picture"
+                                                    class="btn btn-outline-secondary btn-sm ms-1"><i
+                                                        class="fa-solid fa-xmark"></i> Remove profile picture</a>
                                             </form>
                                         </div>
                                     @else
                                         <div class="d-flex justify-content-center mb-2">
-                                            <a href="{{ route('profile.upload_profile_picture', $user->id) }}" class="btn btn-success btn-sm"><i class="fa-solid fa-upload"></i> Upload profile picture</a>
+                                            <a href="{{ route('profile.upload_profile_picture', $user->id) }}"
+                                                class="btn btn-success btn-sm"><i class="fa-solid fa-upload"></i> Upload
+                                                profile picture</a>
                                         </div>
                                     @endif
                                     <div class="d-flex justify-content-center mb-2">
-                                        <a href="{{ route('profile.edit', $user->id) }}" class="btn btn-secondary"><i class="fas fa-user-edit"></i> Edit profile</a>
+                                        <a href="{{ route('profile.edit', $user->id) }}" class="btn btn-secondary"><i
+                                                class="fas fa-user-edit"></i> Edit profile</a>
                                         <a class="btn btn-danger ms-1"><i class="fas fa-user-times"></i> Delete profile</a>
                                     </div>
                                 @endif
                             @else
                                 <div class="d-flex justify-content-center">
-                                    <p class="text-muted"><small>To interact with {{ $user->name }}, please <a style="text-decoration: none;" href="{{ route('authentication.login') }}">log in</a> first.</small></p>
+                                    <p class="text-muted"><small>To interact with {{ $user->name }}, please <a
+                                                style="text-decoration: none;"
+                                                href="{{ route('authentication.login') }}">log in</a> first.</small></p>
                                 </div>
                             @endif
                         </div>
@@ -85,6 +100,14 @@
                                 </div>
                                 <div class="col-sm-9">
                                     <p class="text-muted mb-0">{{ format_date($user->birth_date) }}</p>
+                                    @php
+                                        $today = new DateTime();
+                                        $birthday = new DateTime($user->birth_date);
+                                    @endphp
+
+                                    @if ($today->format('m-d') == $birthday->format('m-d'))
+                                        <small class="birthday-message">Happy birthday {{ $user->name }}! 🎉🎂🎈</small> 
+                                    @endif
                                 </div>
                             </div>
                             <hr>
@@ -104,7 +127,7 @@
                                 </div>
                                 <div class="col-sm-9">
                                     <p class="text-muted mb-0">
-                                        @if($user->bio)
+                                        @if ($user->bio)
                                             {{ $user->bio }}
                                         @else
                                             <i><small>No bio available at the moment!</small></i>
@@ -112,14 +135,17 @@
                                     </p>
                                 </div>
                             </div>
-                            @if(Auth::check())
+                            @if (Auth::check())
                                 @if ($user->id == auth()->user()->id)
                                     <hr>
                                     <div class="row mt-3">
                                         <div class="col-sm-12">
-                                            <p class="text-muted"><small>Please remember that it is your responsibility to ensure all the information on your profile is accurate and up to date. Should you notice any mistakes, feel free to make the necessary corrections.</small></p>
+                                            <p class="text-muted"><small>Please remember that it is your responsibility to
+                                                    ensure all the information on your profile is accurate and up to date.
+                                                    Should you notice any mistakes, feel free to make the necessary
+                                                    corrections.</small></p>
                                         </div>
-                                    </div>        
+                                    </div>
                                 @endif
                             @endif
                         </div>
--- twodeesapp/resources/views/welcome.blade.php
+++ twodeesapp/resources/views/welcome.blade.php
@@ -5,14 +5,18 @@
         <div class="container">
             @include('common._message')
             <div class="intro">
-                <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="{{ asset('home/xbox-playstation-group-0057-64c3e751a896f.jpg') }}" alt="..." />
+                <img class="intro-img img-fluid mb-3 mb-lg-0 rounded"
+                    src="{{ asset('home/xbox-playstation-group-0057-64c3e751a896f.jpg') }}" alt="..." />
                 <div class="intro-text left-0 text-center bg-faded p-5 rounded">
                     <h2 class="section-heading mb-4">
                         <span class="section-heading-upper">Crafted with love for gamers</span>
                         <span class="section-heading-lower">Powered by gamers</span>
                     </h2>
-                    <p class="mb-3">Here, we believe that gaming is about fun and connection, not console wars. Whether you are a fan of PlayStation, Xbox, Nintendo, or PC, every platform is welcome. The goal is simple - explore new titles and celebrate your passion for play!</p>
-                    <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="{{ route('items.index') }}">Explore more today!</a></div>
+                    <p class="mb-3">Here, we believe that gaming is about fun and connection, not console wars. Whether
+                        you are a fan of PlayStation, Xbox, Nintendo, or PC, every platform is welcome. The goal is simple -
+                        explore new titles and celebrate your passion for play!</p>
+                    <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl"
+                            href="{{ route('items.index') }}">Explore more today!</a></div>
                 </div>
             </div>
         </div>
@@ -23,10 +27,13 @@
                 <div class="col-xl-9 mx-auto">
                     <div class="cta-inner bg-faded text-center rounded">
                         <h2 class="section-heading mb-4">
-                            <span class="section-heading-upper">Calling all local gaming enthusiasts and businesses! 🇲🇹</span>
+                            <span class="section-heading-upper">Calling all local gaming enthusiasts and businesses!
+                                🇲🇹</span>
                             <span class="section-heading-lower">All for one</span>
                         </h2>
-                        <p class="mb-0">Let's foster a sense of community among gamers and create unique and memorable experiences that benefit everyone involved. This website is a testament to our eargerness to contribute to the growth and success of our shared gaming culture.</p>
+                        <p class="mb-0">Let's foster a sense of community among gamers and create unique and memorable
+                            experiences that benefit everyone involved. This website is a testament to our eargerness to
+                            contribute to the growth and success of our shared gaming culture.</p>
                     </div>
                 </div>
             </div>
@@ -38,14 +45,41 @@
                 <div class="product-item-title d-flex">
                     <div class="bg-faded p-5 d-flex ms-auto rounded">
                         <h2 class="section-heading mb-0">
-                            <span class="section-heading-upper">It's a good day to play. When everyone plays, we all win.</span>
+                            <span class="section-heading-upper">It's a good day to play. When everyone plays, we all
+                                win.</span>
                             <span class="section-heading-lower">Microsoft Xbox <small>(2001)</small></span>
                         </h2>
                     </div>
                 </div>
-                <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="{{ asset('home/xbox_og.jpg') }}" alt="..." />
+                <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0"
+                    src="{{ asset('home/xbox_og.jpg') }}" alt="..." />
                 <div class="product-item-description d-flex me-auto">
-                    <div class="bg-faded p-5 rounded"><p class="mb-0">Throwback to Microsoft's foray into the console gaming market, often associated with the famous alien-like look and feel and the real-time procedurally generated startup on 64 megabytes worth of memory unit.</p></div>
+                    <div class="bg-faded p-5 rounded">
+                        <p class="mb-0">Throwback to Microsoft's foray into the console gaming market, often associated
+                            with the famous alien-like look and feel and the real-time procedurally generated startup on 64
+                            megabytes worth of memory unit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="product-item mt-4">
+                <div class="product-item-title d-flex">
+                    <div class="bg-faded p-5 d-flex me-auto rounded">
+                        <h2 class="section-heading mb-0">
+                            <span class="section-heading-upper">The Legend Continues</span>
+                            <span class="section-heading-lower">Sony PlayStation 2 <small>(2000)</small></span>
+                        </h2>
+                    </div>
+                </div>
+                <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0"
+                    src="{{ asset('home/jjnNLgLZ7vij5VeRLsaTRi.jpg') }}" alt="Sony PlayStation 2" />
+                <div class="product-item-description d-flex ms-auto">
+                    <div class="bg-faded p-5 rounded">
+                        <p class="mb-0">
+                            Step into the world of the PlayStation 2, the best-selling console of all time. Known for its
+                            iconic design, massive game library, and the unforgettable sound of the disc tray, it brought
+                            families and friends together for countless adventures.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
